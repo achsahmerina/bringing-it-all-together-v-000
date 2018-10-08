b1d1c0dcaf9025e7d2911aae53961cd3b8bb67a5
Achsah Abraham <achsahmp@gmail.com>
2018-10-08 23:46:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -31,7 +31,7 @@ class Dog
 
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-      save
+      self
 
     end
   end
