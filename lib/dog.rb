34a312e952745152d544b1d6084de5bcdbdb3213
Achsah Abraham <achsahmp@gmail.com>
2018-10-08 23:54:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,7 +46,7 @@ class Dog
       SELECT * FROM dogs WHERE id = ?
     SQL
 
-    object = DB[:conn].execute(sql, id)
+    object = DB[:conn].execute(sql, id).first
     Dog.new(id: object[0], name: object[1], breed: object[2])
   end
 end
